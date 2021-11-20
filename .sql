5208c94d2fde0c292165caa8781163b5a5445a86
benrobo <alumonabenaiah71@gmail.com>
2021-11-20 12:18:34 +0100

    transition from sequelize to normal procedural sql standars

--- .sql
+++ .sql
@@ -1,1 +1,32 @@
 CREATE DATABASE kwickquick;
+
+
+-- organization table
+
+
+CREATE TABLE organizations(
+    id TEXT UNIQUE NOT NULL PRIMARY KEY,
+    "orName" TEXT NOT NULL,
+    "orMail" TEXT NOT NULL UNIQUE,
+    "orPassword" TEXT NOT NULL,
+    "orLogo" TEXT NOT NULL,
+    "orNumber" INT NOT NULL,
+    "refreshToken" TEXT NOT NULL,
+    "orEntryDate" TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+
+-- product table
+
+CREATE TABLE products(
+    id TEXT UNIQUE NOT NULL PRIMARY KEY,
+    "orId" TEXT NOT NULL,
+    "pName" TEXT NOT NULL,
+    "pPrice" TEXT NOT NULL UNIQUE,
+    "pNumber" TEXT NOT NULL,
+    "pImage" TEXT NOT NULL,
+    "orNumber" INT NOT NULL,
+    "orName" TEXT NOT NULL,
+    "hash" TEXT NOT NULL,
+    "pDate" TIMESTAMP NOT NULL DEFAULT NOW()
+);
--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 require("dotenv").config()
 const express = require('express');
-const path = require('path');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
-const conn = require("./config/db")
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+
+Organization.sequelize.sync({ force: true })
 
 const PORT = process.env.NODE_ENV | 5000
 
